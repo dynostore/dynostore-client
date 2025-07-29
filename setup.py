ff1cb51ef3725b0e6fca03cc0c1f50fd4f755d8c
ddomizzisg <ddomizzisag@gmail.com>
2025-07-29 14:42:23 +0200

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dynostore",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     install_requires=["requests", "cryptography"],
     entry_points={
